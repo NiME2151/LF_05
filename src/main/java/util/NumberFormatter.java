0d229d8703659a55922160ce9ca6b5eeebac581f
Nico Meyer <nicomeyer@MacBook-Air-von-Nico.local>
2023-01-16 09:58:22 +0100

    ÄNDERUNG updated NumberFormatter

--- src/main/java/util/NumberFormatter.java
+++ src/main/java/util/NumberFormatter.java
@@ -12,13 +12,7 @@ public class NumberFormatter {
         return df.format(toBeFormatted);
     }
 
-    public static String format(double toBeFormatted, String pattern, Locale locale) {
-        DecimalFormat df = (DecimalFormat) NumberFormat.getNumberInstance(locale);
-        df.applyPattern(pattern);
-        return df.format(toBeFormatted);
-    }
-
-    public static String format(float toBeFormatted, String pattern, Locale locale) {
+    public static <Type> String format(Type toBeFormatted, String pattern, Locale locale) {
         DecimalFormat df = (DecimalFormat) NumberFormat.getNumberInstance(locale);
         df.applyPattern(pattern);
         return df.format(toBeFormatted);
@@ -30,13 +24,7 @@ public class NumberFormatter {
         return df.format(toBeFormatted);
     }
 
-    public static String formatToGermanNumber(double toBeFormatted, int decimalPlace) {
-        DecimalFormat df = (DecimalFormat) NumberFormat.getNumberInstance(Locale.GERMAN);
-        df.applyPattern("#." + "#".repeat(decimalPlace));
-        return df.format(toBeFormatted);
-    }
-
-    public static String formatToGermanNumber(float toBeFormatted, int decimalPlace) {
+    public static <Type> String formatToGermanNumber(Type toBeFormatted, int decimalPlace) {
         DecimalFormat df = (DecimalFormat) NumberFormat.getNumberInstance(Locale.GERMAN);
         df.applyPattern("#." + "#".repeat(decimalPlace));
         return df.format(toBeFormatted);
@@ -46,19 +34,7 @@ public class NumberFormatter {
         return NumberFormat.getCurrencyInstance(locale).format(Double.parseDouble(toBeFormatted));
     }
 
-    public static String formatToCurrency(double toBeFormatted, Locale locale) {
-        return NumberFormat.getCurrencyInstance(locale).format(toBeFormatted);
-    }
-
-    public static String formatToCurrency(int toBeFormatted, Locale locale) {
-        return NumberFormat.getCurrencyInstance(locale).format(toBeFormatted);
-    }
-
-    public static String formatToCurrency(long toBeFormatted, Locale locale) {
-        return NumberFormat.getCurrencyInstance(locale).format(toBeFormatted);
-    }
-
-    public static String formatToCurrency(float toBeFormatted, Locale locale) {
+    public static <Type> String formatToCurrency(Type toBeFormatted, Locale locale) {
         return NumberFormat.getCurrencyInstance(locale).format(toBeFormatted);
     }
 }
