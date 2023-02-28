ea5233751d659e7642ea90e3a76beb988981b34a
Nico Meyer <nicomeyer@MacBook-Air-von-Nico.local>
2023-02-28 08:51:01 +0100

    ADD singleton pattern task

---
+++ src/main/java/ab/objektorientierte_programmierung/singleton_pattern/OptionenSingleton.java
@@ -0,0 +1,27 @@
+package ab.objektorientierte_programmierung.singleton_pattern;
+
+class OptionenSingleton {
+
+    private static OptionenSingleton instance;
+    private String saveLocation;
+
+    private OptionenSingleton() {
+
+    }
+
+    public static OptionenSingleton getInstance() {
+        if (instance == null) {
+            instance = new OptionenSingleton();
+            return instance;
+        }
+        return instance;
+    }
+
+    public String getSaveLocation() {
+        return saveLocation;
+    }
+
+    public void setSaveLocation(String saveLocation) {
+        this.saveLocation = saveLocation;
+    }
+}
---
+++ src/main/java/ab/objektorientierte_programmierung/singleton_pattern/Runner.java
@@ -0,0 +1,13 @@
+package ab.objektorientierte_programmierung.singleton_pattern;
+
+class Runner {
+
+    public static void main(String[] args) {
+
+        String path = "fakePath";
+        OptionenSingleton options = OptionenSingleton.getInstance();
+        System.out.println(OptionenSingleton.getInstance().getSaveLocation());
+        options.setSaveLocation(path);
+        System.out.println(OptionenSingleton.getInstance().getSaveLocation());
+    }
+}
